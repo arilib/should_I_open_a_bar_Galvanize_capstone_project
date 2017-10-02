c02b8c6e105d868bfb2295115eb7cb52fb57d94d
arilib <arilib@users.noreply.github.com>
2017-10-02 16:38:12 -0600

    2017-10-02

--- src/model_training_and_scoring.py
+++ src/model_training_and_scoring.py
@@ -4,27 +4,33 @@ import pickle
 from sklearn.linear_model import LinearRegression
 from sklearn.linear_model import Ridge
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import mean_squared_error
-from sklearn.metrics import r2_score
-from sklearn.metrics import explained_variance_score
+from sklearn.model_selection import cross_val_score, cross_val_predict
+import matplotlib.pyplot as plt
+import warnings
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.tree import DecisionTreeRegressor
+
 
 def split_data(filename):
     df = pd.read_csv(filename)
     df = df.drop('Unnamed: 0', axis=1)
     county_info_df = df.copy()
-    y = df.pop('bars')
+    df = df[df['bars'] >= 3]
+    df = df[df['hotels'] >= 3]
+    y = np.log(df.pop('bars'))
     X = df.drop(['geo_id', 'state_name', 'state_code', 'county_name'], axis=1)
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 7)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.1, random_state = 7)
     cols = X_train.columns
+    # print(cols)
     return(X_train, X_test, y_train, y_test, county_info_df, cols)
 
 def lin_reg(X_train, X_test, y_train, y_test):
     lr = LinearRegression(fit_intercept=True)
     model = lr.fit(X_train,y_train)
-    y_train_pred = model.predict(X_train)
+    y_train_pred = model.predict(X_train.values)
     y_test_pred = model.predict(X_test)
-    train_score = r2_score(y_train, y_train_pred)
-    test_score = r2_score(y_test, y_test_pred)
+    train_score = cross_val_score(model, y_train.reshape(-1,1), y_train_pred.reshape(-1,1), cv = 5)
+    test_score = cross_val_score(model, y_test.reshape(-1,1), y_test_pred.reshape(-1,1), cv = 5)
     return(model, train_score, test_score)
 
 def rid_reg(X_train, X_test, y_train, y_test):
@@ -32,8 +38,26 @@ def rid_reg(X_train, X_test, y_train, y_test):
     model = rr.fit(X_train,y_train)
     y_train_pred = model.predict(X_train)
     y_test_pred = model.predict(X_test)
-    train_score = r2_score(y_train, y_train_pred)
-    test_score = r2_score(y_test, y_test_pred)
+    train_score = cross_val_score(model, y_train.reshape(-1,1), y_train_pred.reshape(-1,1), cv = 5)
+    test_score = cross_val_score(model, y_test.reshape(-1,1), y_test_pred.reshape(-1,1), cv = 5)
+    return(model, train_score, test_score)
+
+def tr_reg(X_train, X_test, y_train, y_test):
+    regr = DecisionTreeRegressor(random_state = 7)
+    model = regr.fit(X_train, y_train)
+    y_train_pred = model.predict(X_train)
+    y_test_pred = model.predict(X_test)
+    train_score = cross_val_score(model, y_train.reshape(-1,1), y_train_pred.reshape(-1,1), cv = 5)
+    test_score = cross_val_score(model, y_test.reshape(-1,1), y_test_pred.reshape(-1,1), cv = 5)
+    return(model, train_score, test_score)
+
+def rf_reg(X_train, X_test, y_train, y_test):
+    regr = RandomForestRegressor(n_estimators=10, max_depth=2, oob_score=True, n_jobs = -1, random_state = 7)
+    model = regr.fit(X_train, y_train)
+    y_train_pred = model.predict(X_train)
+    y_test_pred = model.predict(X_test)
+    train_score = cross_val_score(model, y_train.reshape(-1,1), y_train_pred.reshape(-1,1), cv = 5)
+    test_score = cross_val_score(model, y_test.reshape(-1,1), y_test_pred.reshape(-1,1), cv = 5)
     return(model, train_score, test_score)
 
 def store_model_info(county_info_df, model, cols):
@@ -44,15 +68,46 @@ def store_model_info(county_info_df, model, cols):
     output.close()
     return
 
+def actual_pred_plot(mp, model, X_test, y_test, filename):
+    y = y_test.reshape(-1,1)
+    predicted = cross_val_predict(model, X_test, y, cv = 5)
+    fig, ax = plt.subplots()
+    ax.scatter(y, predicted, edgecolors=(0, 0, 0), alpha=0.3)
+    ax.plot([y.min(), y.max()], [y.min(), y.max()], 'k--', lw=4)
+    ax.set_xlabel('Actual number of bars')
+    ax.set_ylabel('Predicted number of bars')
+    ax.set_label(filename)
+    plt.savefig('../figures/'+mp+filename[9:-3]+'png')
+    return()
+
 if __name__ == '__main__':
-    filename = '../data/2015_toy_sd_rnd_nan_to_min.csv'
+    warnings.simplefilter(action='ignore')
+    filename = '../data/2015_lin_sd_rnd_nan_to_min.csv'
     X_train, X_test, y_train, y_test, county_info_df, cols = split_data(filename)
+
+    list_of_models = [('lr', 'Linear Regression', 'lr_model'), ('rr', 'Ridge Regression', 'rr_model'), ('tr', 'Decision Tree Regressor', 'tr_model'), ('rr', 'Random Forest Regressor', 'rf_model')]
+
+    for model in list_of_models:
+        print(model[0], model[1], model[2])
+
+
     lr_model, lr_train_score, lr_test_score = lin_reg(X_train, X_test, y_train, y_test)
-
-
-
-    print('Linear Regression Score\nTrain: {0}\nTest: {1}\n'. format(lr_train_score, lr_test_score))
     rr_model, rr_train_score, rr_test_score = rid_reg(X_train, X_test, y_train, y_test)
-    print('Ridge Regression Score\nTrain: {0}\nTest: {1}\n'. format(rr_train_score, rr_test_score))
+    tr_model, tr_train_score, tr_test_score = tr_reg(X_train, X_test, y_train, y_test)
+    rf_model, rf_train_score, rf_test_score = rf_reg(X_train, X_test, y_train, y_test)
+
+
+    print(filename)
+    print('Linear Regression Score\nTrain: {0}\nTest: {1}\n'. format(round(lr_train_score.mean(),3), round(lr_test_score.mean(),3)))
+    actual_pred_plot('lr', lr_model, X_test, y_test, filename)
+
+    print('Ridge Regression Score\nTrain: {0}\nTest: {1}\n'. format(round(rr_train_score.mean(),3), round(rr_test_score.mean(),3)))
+    actual_pred_plot('rr', rr_model, X_test, y_test, filename)
+
+    print('Decision Tree Regressor Score\nTrain: {0}\nTest: {1}\n'. format(round(rf_train_score.mean(),3), round(rf_test_score.mean(),3)))
+    actual_pred_plot('tr', tr_model, X_test, y_test, filename)
+
+    print('Random Forest Regressor Score\nTrain: {0}\nTest: {1}\n'. format(round(rf_train_score.mean(),3), round(rf_test_score.mean(),3)))
+    actual_pred_plot('rf', rf_model, X_test, y_test, filename)
 
     store_model_info(county_info_df, rr_model, cols)
